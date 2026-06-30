78001933f8d1e60a555ee106d5a1a7d5bdf1b770
mario <mario_reyes505@hotmail.com>
2026-06-29 21:15:58 -0500

    add action to remove item on CartContext, wrapped App.jsx around cartContextProvider , and then used cartContext on MealItem to add items to cart, and on Header.jsx to show number of items on cart

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -30,18 +30,47 @@ function cartReducer(state, action) {
   }
   if (action.type === "REMOVE_ITEM") {
     // remove items from state
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id,
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updateItems = [...state.items];
+    if (existingCartItem.quantity === 1) {
+      updateItems.splice(existingCartItemIndex, 1);
+    } else {
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity - 1,
+      };
+      updateItems[existingCartItemIndex] = updatedItem;
+    }
+
+    return { ...state, items: updateItems };
   }
 
   return state;
 }
 
 export function CartContextProvider({ children }) {
-  useReducer(cartReducer, {
+  const [cart, dispatchCartAction] = useReducer(cartReducer, {
     items: [],
-    addItem: (item) => {},
-    removeItem: (id) => {},
   });
-  return <CartContext>{children}</CartContext>;
+
+  function addItem(item) {
+    dispatchCartAction({ type: "ADD_ITEM", item: item });
+  }
+
+  function removeItem(id) {
+    dispatchCartAction({ type: "REMOVE_ITEM", id: id });
+  }
+
+  const cartContext = { items: cart.items, addItem, removeItem };
+
+  console.log(cartContext);
+
+  return <CartContext value={cartContext}>{children}</CartContext>;
 }
 
 export default CartContext;
