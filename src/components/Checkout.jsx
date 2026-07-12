461bbdfaac1244966ef1ff115cd0ddbc5c98935d
mario <mario_reyes505@hotmail.com>
2026-07-11 21:07:41 -0500

    update logic in Checkout.jsx to use form actions to handle submitting form data to backend

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useActionState, useContext } from "react";
 import Modal from "./UI/Modal";
 import CartContext from "../store/CartContext";
 import { currencyFormatter } from "../util/formatting";
@@ -19,13 +19,10 @@ export default function Checkout() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
 
-  const {
-    data,
-    isLoading: isSending,
-    error,
-    sendRequest,
-    clearData,
-  } = useHttp("http://localhost:3000/orders", requestConfig);
+  const { data, error, sendRequest, clearData } = useHttp(
+    "http://localhost:3000/orders",
+    requestConfig,
+  );
 
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
@@ -42,18 +39,20 @@ export default function Checkout() {
     clearData();
   }
 
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    const fd = new FormData(event.target);
+  async function checkoutAction(prevState, fd) {
     const customerData = Object.fromEntries(fd.entries()); //{email: test@example.com}
 
-    sendRequest(
+    await sendRequest(
       JSON.stringify({
         order: { items: cartCtx.items, customer: customerData },
       }),
     );
   }
+
+  const [formState, formAction, isSending] = useActionState(
+    checkoutAction,
+    null,
+  );
 
   let actions = (
     <>
@@ -89,7 +88,7 @@ export default function Checkout() {
 
   return (
     <Modal open={userProgressCtx.progress === "checkout"} onClose={handleClose}>
-      <form onSubmit={handleSubmit}>
+      <form action={formAction}>
         <h2>Checkout</h2>
         <p>Total Amount: {currencyFormatter.format(cartTotal)}</p>
 
